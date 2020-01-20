c858fab173beb7aacd8d39ca4172d8c6ef9d8b0f
nraiha <nraiha@student.oulu.fi>
2020-01-20 19:53:49 +0200

    Added a button response

--- app/src/main/java/com/example/mobocomp/MainActivity.kt
+++ app/src/main/java/com/example/mobocomp/MainActivity.kt
@@ -3,10 +3,21 @@ package com.example.mobocomp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.Toast
+import android.widget.Button
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val button_click = findViewById(R.id.button_click) as Button
+        // set on-click listener
+        button_click.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "Click clack.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
