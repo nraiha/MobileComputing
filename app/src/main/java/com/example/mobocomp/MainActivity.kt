913e60cf13d7c29ee58201ab90ccb0a379afe181
Nuutti Räihä <nraiha@Nuuttis-MacBook-Pro.local>
2020-01-27 09:57:28 +0200

    New activities and 3 FABS with listeners

--- app/src/main/java/com/example/mobocomp/MainActivity.kt
+++ app/src/main/java/com/example/mobocomp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.mobocomp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.Toast
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,12 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get reference to button
         val button_click = findViewById(R.id.button_click) as Button
         // set on-click listener
         button_click.setOnClickListener {
             // your code to perform when the user clicks on the button
-            Toast.makeText(this@MainActivity, "Click clack.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity,"Click clack.", Toast.LENGTH_SHORT).show()
         }
+
+        var fabOpened = false
+
+        fab1.setOnClickListener {
+            if (!(fabOpened)) {
+                fabOpened = true
+                fab_map.animate().translationY(-resources.getDimension(R.dimen.standard_66))
+                fab_time.animate().translationY(-resources.getDimension(R.dimen.standard_166))
+            } else {
+                fabOpened = false
+                fab_map.animate().translationY(0f)
+                fab_time.animate().translationY(0f)
+            }
+        }
+
+        fab_time.setOnClickListener {
+            val intent = Intent(applicationContext, TimeActivity::class.java)
+            startActivity(intent)
+        }
+
+        fab_map.setOnClickListener {
+            val intent = Intent(applicationContext, MapActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
