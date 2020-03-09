9e9b32e75bd0018deb3ac3bd5b84bf2a3bac33d0
Nuutti Räihä <nuutti.raiha@gmail.com>
2020-03-09 19:13:16 +0200

    Lab6

--- app/src/main/java/com/example/mobocomp/MapActivity.kt
+++ app/src/main/java/com/example/mobocomp/MapActivity.kt
@@ -1,14 +1,15 @@
 package com.example.mobocomp
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -16,7 +17,10 @@ import kotlinx.android.synthetic.main.activity_map.*
 import org.jetbrains.anko.toast
 import java.util.*
 import androidx.room.Room
+import com.google.android.gms.location.*
 import org.jetbrains.anko.doAsync
+import android.graphics.Color
+
 
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -25,14 +29,18 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var fusedLocationClient: FusedLocationProviderClient
     lateinit var selectedLocation: LatLng
 
+    lateinit var geofencingClient: GeofencingClient
+    val GEOFENCE_ID = "REMINDER_GEOFENCE_ID"
+    val GEOFENCE_RADIUS = 500
+    val GEOFENCE_EXPIRATION = 120*24*60*60*1000
+    val GEOFENCE_DWELL_DELAY = 2*60*1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-
         (map_fragment as SupportMapFragment).getMapAsync(this)
-
+        geofencingClient=LocationServices.getGeofencingClient(this)
         map_create.setOnClickListener {
-
             val reminderText = et_message.text.toString()
             if (reminderText.isEmpty()) {
                 toast("Please provide reminder text")
@@ -53,20 +61,60 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             )
 
             doAsync{
-                val db = Room.databaseBuilder(applicationContext,
-                        AppDatabase::class.java, "reminders").build()
-                db.reminderDao().insert(reminder)
+                val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "reminders").build()
+                val uid = db.reminderDao().insert(reminder).toInt()
+                reminder.uid=uid
+
                 db.close()
+                CreateGeoFence(selectedLocation, reminder, geofencingClient)
             }
             finish()
         }
     }
 
+    private fun CreateGeoFence(selectedLocation: LatLng, reminder: Reminder, geofencingClient: GeofencingClient) {
+        val geofence = Geofence.Builder().setRequestId(GEOFENCE_ID).setCircularRegion(
+                selectedLocation.latitude,
+                selectedLocation.longitude,
+                GEOFENCE_RADIUS.toFloat()).setExpirationDuration(
+                    GEOFENCE_EXPIRATION.toLong()).setTransitionTypes(
+                        Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_DWELL)
+                            .setLoiteringDelay(GEOFENCE_DWELL_DELAY).build()
+        val geofenceRequest = GeofencingRequest.Builder().setInitialTrigger(
+                Geofence.GEOFENCE_TRANSITION_ENTER).addGeofence(geofence).build()
+
+        val intent = Intent(
+            this, GeoFenceReceiver::class.java).putExtra("uid", reminder.uid)
+            .putExtra("message", reminder.message)
+            .putExtra("location", reminder.location)
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        geofencingClient.addGeofences(geofenceRequest,pendingIntent)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (grantResults.isEmpty() && (grantResults[0]==PackageManager.PERMISSION_DENIED
+                    || grantResults[1] == PackageManager.PERMISSION_DENIED)) {
+            toast("The reminder needs all the permissions to function")
+        }
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.Q) {
+            if (grantResults.isNotEmpty() && (grantResults[2] == PackageManager.PERMISSION_DENIED)){
+                toast("The reminder needs all the permissions to function")
+            }
+        }
+    }
     override fun onMapReady(map: GoogleMap?) {
         gMap = map ?: return
         if (ContextCompat.checkSelfPermission(
-                this, android.Manifest.permission.ACCESS_FINE_LOCATION )
-                                                            == PackageManager.PERMISSION_GRANTED
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                                                                == PackageManager.PERMISSION_GRANTED
             || (ContextCompat.checkSelfPermission(
                 this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
                                                             == PackageManager.PERMISSION_GRANTED)) {
@@ -83,6 +131,14 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
             }
         } else {
+            var permission = mutableListOf<String>()
+            permission.add(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            permission.add(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+                permission.add(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
+
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(
@@ -101,8 +157,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 var title = ""
                 var city = ""
                 try {
-                    val addressList =
-                        geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                    val addressList = geocoder.getFromLocation(
+                        location.latitude, location.longitude, 1)
                     city = addressList.get(0).locality
                     title = addressList.get(0).getAddressLine(0)
                 } catch (e:Exception){ }
@@ -110,6 +166,10 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 val marker =
                     addMarker(MarkerOptions().position(location).snippet(title).title(city))
                 marker.showInfoWindow()
+
+                addCircle(com.google.android.gms.maps.model.CircleOptions().center(location)
+                    .strokeColor(Color.argb(50, 70, 70, 70))
+                    .fillColor(Color.argb(100, 150, 150, 150)))
                 selectedLocation = location
             }
         }
--- app/src/main/java/com/example/mobocomp/ReminderReceiver.kt
+++ app/src/main/java/com/example/mobocomp/ReminderReceiver.kt
@@ -3,11 +3,21 @@ package com.example.mobocomp
 import android.content.BroadcastReceiver
 import android.content.Intent
 import android.content.Context
+import androidx.room.Room
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 
 class ReminderReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val text = intent.getStringExtra("message")
-        context.toast(text!!)
+        val uid = intent.getIntExtra("uid", 0)
+
+        MainActivity.showNotification(context,text!!)
+
+        doAsync{
+            val db = Room.databaseBuilder(context, AppDatabase::class.java, "reminder").build()
+            db.reminderDao().delete(uid)
+            db.close()
+        }
     }
 }
